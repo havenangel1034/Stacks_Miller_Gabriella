0dbd93fe005810145fcc4a2d7aa744c4ab06528c
Gabby <gabbie10112005@gmail.com>
2025-02-05 12:56:52 -0600

    Initial commit

---
+++ Stacks_Miller_Gabriella/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_Miller_Gabriella
+{
+    internal class Card
+    {
+
+
+        public string Number { get; }
+
+        public string Suit { get; }
+
+        public Card(string numParam, string suitParam)
+        {
+            Number = numParam;
+            Suit = suitParam;
+        }
+    }
+}
